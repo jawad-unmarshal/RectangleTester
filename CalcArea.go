3f7b909f4cfae3f9bd3ff07c9dc6957975accf9d
Jawad Ahmed Khan <88373522+jawad-unmarshal@users.noreply.github.com>
2021-08-19 13:19:38 +0530

    Added function to calculate Perimeter, Peri+Area. Renamed parameters in RectArea to be more descriptive and meaningful.

--- CalcArea.go
+++ CalcArea.go
@@ -1,5 +1,13 @@
 package RectangleTester
 
-func RectArea(a, b float64) float64 {
-	return a * b
+func RectArea(length, breadth float64) float64 {
+	return length * breadth
+}
+func RectPerimeter(length, breadth float64) float64 {
+	return 2. * (length + breadth)
+}
+
+func SumAreaPeri(length, breadth float64) float64 {
+	return RectArea(length, breadth) + RectPerimeter(length, breadth)
+
 }
